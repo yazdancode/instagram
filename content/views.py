22cf079dd0f50b079e084a8c072230f7154d0736
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-08-28 23:56:18 -0700

    توضیح تغییرات

--- content/views.py
+++ content/views.py
@@ -29,7 +29,7 @@ class TagListAPI(APIView):
     def post(self, request, *args, **kwargs):
         serializer = TagListSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()  # 'instance' variable is unnecessary here
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
