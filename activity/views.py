ad774210b08695bc8089ad5292eacf0171deb745
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-08-29 04:49:34 -0700

    CommentCreateAPIView

--- activity/views.py
+++ activity/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from rest_framework.decorators import permission_classes
 from rest_framework.generics import CreateAPIView
-from rest_framework.settings import perform_import
+from rest_framework.permissions import IsAuthenticated
 
 from activity.models import Comment
 from activity.serializers import CommentCreateSerializer
@@ -10,9 +9,10 @@ from activity.serializers import CommentCreateSerializer
 class CommentCreateAPIView(CreateAPIView):
 	queryset = Comment.objects.all()
 	serializer_class = CommentCreateSerializer
-    
-    
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+	permission_classes = (IsAuthenticated,)
+	
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
+		
         
 	
