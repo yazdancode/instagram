9af43a2d4b1bc4707acf4ef22d1d16605c6e3b26
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-08-29 01:44:14 -0700

    remove post

--- content/views.py
+++ content/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -17,13 +17,12 @@ class TagDetailAPI(APIView):
         serializer = TagDetailSerializer(tag)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class TagListAPI(ListAPIView):
+class TagListAPI(ListCreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagListSerializer
     permission_classes = (IsAuthenticated,)
 
 class PostDetailAPI(APIView):
-    permission_classes = (IsAuthenticated,)
 
     def get(self, request, pk, *args, **kwargs):
         post = get_object_or_404(Post, pk=pk)
