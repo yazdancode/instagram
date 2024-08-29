c7101fc7fcb1c552759cb1b58da16316f1e3cfb3
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-08-29 01:22:31 -0700

    .pyc

--- content/views.py
+++ content/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
